43af27b69ec08709d6ca7b33c6b37dcda1b071b3
M.Kristóf <drzero123456@gmail.com>
2022-03-17 20:07:18 +0100

    Grid generator added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const grid_container = document.querySelector(".grid-container");
 
 
-function generate_gird () {
-    for (let i = 0; i < 25*25; i++) {
+function generate_grid (input) {
+    grid_container.style.cssText = `
+    display: grid;   
+    grid-template-columns: repeat(${input},1fr);
+    grid-template-rows: repeat(${input},1fr);
+    `
+    
+
+    for (let i = 0; i < Math.pow(input, 2); i++) {
         div_element = document.createElement("div")
         div_element.classList.add("grid-item");
-        grid_container.appendChild(div_element); 
+        grid_container.appendChild(div_element);
     }
+
     
 }
 
+generate_grid(20)
