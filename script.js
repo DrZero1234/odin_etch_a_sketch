43f49c5a9469c8479c49c95dd8ad61dd1b92f957
M.Kristóf <drzero123456@gmail.com>
2022-03-22 19:43:56 +0100

    ui changes and js script changes

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
 const grid_container = document.querySelector(".grid-container");
 const ui = document.querySelector(".UI");
-clrBttn = document.createElement("button")
-clrBttn.innerText = "Clear";
-ui.appendChild(clrBttn);
-clearBttn = document.querySelector("button");
+const wrapper = document.querySelector(".wrapper");
+const default_value = 16;
+const clearBttn = document.querySelector("#clear");
+let brush_input = document.querySelector("#brush-color")
+let cols = document.querySelector("#cols-num");
+let rows = document.querySelector("#rows-num");
 
-function generate_grid (input) {
 
+
+function generate_grid (rows = default_value,cols = default_value) {
+    while (grid_container.firstChild) {
+        grid_container.removeChild(grid_container.firstChild)
+    }
     grid_container.style.cssText = `
     display: grid;   
-    grid-template-columns: repeat(${input},1fr);
-    grid-template-rows: repeat(${input},1fr);
+    grid-template-columns: repeat(${cols},1fr);
+    grid-template-rows: repeat(${cols},auto);
     `
     
 
-    for (let i = 0; i < Math.pow(input, 2); i++) {
+    for (let i = 0; i < cols*rows; i++) {
         div_element = document.createElement("div")
         div_element.classList.add("grid-item");
         grid_container.appendChild(div_element);
@@ -24,19 +30,34 @@ function generate_grid (input) {
 
 generate_grid(16)
 
+
+
 const all_grid_items = document.querySelectorAll(".grid-item");
 
+cols.addEventListener("oninput", generate_grid(+rows.value, +cols.value))
+cols.addEventListener("oninput", generate_grid(+rows.value, +cols.value))
+brush_input.addEventListener("change", brush_color(brush_input.value));
 
-color_prompt = prompt("Enter a color: ","");
 
-all_grid_items.forEach((grid_item) => {
-    clearBttn.addEventListener("click", () =>{
-        grid_item.style.backgroundColor = "white"
+function brush_color (color) {
+    all_grid_items.forEach((grid_item) => {
+        grid_item.addEventListener("mouseenter", () =>  {
+            grid_item.style.backgroundColor = color;
+        })
     })
-    grid_item.addEventListener("mouseenter", () =>  {
-        grid_item.style.backgroundColor = color_prompt.toLowerCase();
+}
+
+
+function clear() {
+    all_grid_items.forEach((grid_item) => {
+        clearBttn.addEventListener("click", () =>{
+            grid_item.style.backgroundColor = "white"
+        })
     })
-})
+}
+
+clear()
+
 
 
 
