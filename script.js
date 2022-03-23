8dabd2b03aa0b899b45cb44323973bd5c77a2f92
M.Kristóf <drzero123456@gmail.com>
2022-03-23 19:39:24 +0100

    Grid generator works

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function generate_grid (rows = default_value,cols = default_value) {
     grid_container.style.cssText = `
     display: grid;   
     grid-template-columns: repeat(${cols},1fr);
-    grid-template-rows: repeat(${cols},auto);
+    grid-template-rows: repeat(${rows},1fr);
     `
     
 
@@ -30,6 +30,7 @@ function generate_grid (rows = default_value,cols = default_value) {
         div_element.classList.add("grid-item");
         grid_container.appendChild(div_element);
     }
+    console.log("Grid generated")
 }
 
 
@@ -37,8 +38,13 @@ function generate_grid (rows = default_value,cols = default_value) {
 
 
 
-cols.addEventListener("mouseup", generate_grid(Math.floor(+rows.value), Math.floor(+cols.value)))
-cols.addEventListener("mouseup", generate_grid(Math.floor(+rows.value), Math.floor(+cols.value)))
+cols.addEventListener("input", function () {
+    generate_grid(Math.floor(cols.value), Math.floor(this.value))
+    
+})
+rows.addEventListener("input", function () {
+    generate_grid(Math.floor(this.value), Math.floor(rows.value))
+})
 
 const all_grid_items = document.querySelectorAll(".grid-item");
 
@@ -60,7 +66,7 @@ function clear() {
 
 clear()
 
-
+generate_grid(default_value,default_value)
 
 
 // TODO Button on top to clear the grid and ask for a new one with number of columns and rows
